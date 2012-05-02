ef062525a5bc348f441544314724d9e19fea1fcd
Matthew Weier O'Phinney <matthew@zend.com>
2012-05-02 11:21:05 -0500

    Updated view to use new Form API
    - Module adds Form view helpers when it detects this module is selected
    - View script uses new Form view helpers

--- Module.php
+++ Module.php
@@ -2,10 +2,24 @@
 
 namespace PhlyContact;
 
+use Zend\Form\View\HelperLoader as FormHelperLoader;
 use Zend\Module\Consumer\AutoloaderProvider;
 
 class Module implements AutoloaderProvider
 {
+    public function init($manager)
+    {
+        $events = $manager->getEvents();
+        if (method_exists($events, 'getStaticConnections')) {
+            $shared = $events->getStaticConnections();
+        } elseif (method_exists($events, 'getSharedCollections')) {
+            $shared = $events->getSharedCollections();
+        } else {
+            return;
+        }
+
+        $shared->attach('bootstrap', 'bootstrap', array($this, 'onBootstrap'));
+    }
     public function getAutoloaderConfig()
     {
         return array(
@@ -23,5 +37,25 @@ class Module implements AutoloaderProvider
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function onBootstrap($e)
+    {
+        $app     = $e->getParam('application');
+        $locator = $app->getLocator();
+        $this->helperLoader = $locator->get('Zend\View\HelperLoader');
+
+        $app->events()->attach('route', array($this, 'onRouteFinish'), -100);
+    }
+
+    public function onRouteFinish($e)
+    {
+        $matches = $e->getRouteMatch();
+        $controller = $e->getParam('controller');
+        $namespace = substr($controller, 0, strpos($controller, '\\'));
+        if ($namespace !== __NAMESPACE__) {
+            return;
+        }
+        $this->helperLoader->registerPlugins(new FormHelperLoader());
     }
 }
