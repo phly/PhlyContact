2b268aaa4b50426c229f7560f2ce7762366c4e7b
Matthew Weier O'Phinney <matthew@zend.com>
2012-05-10 15:59:58 -0500

    Updated to rev f98caf8b of weierophinney/feature/forms
    - Allows adding proper captcha adapter and csrf

--- src/PhlyContact/Form/ContactForm.php
+++ src/PhlyContact/Form/ContactForm.php
@@ -2,7 +2,7 @@
 namespace PhlyContact\Form;
 
 use Zend\Captcha\Adapter as CaptchaAdapter;
-use Zend\Form\Factory as FormFactory;
+use Zend\Form\Element;
 use Zend\Form\Form;
 
 class ContactForm extends Form
@@ -10,69 +10,55 @@ class ContactForm extends Form
     protected $captchaAdapter;
     protected $csrfToken;
 
-    public function __construct($name = null, CaptchaAdapter $captchaAdapter = null, $csrfToken = null)
+    public function __construct($name = null, CaptchaAdapter $captchaAdapter = null)
     {
         parent::__construct($name);
 
         if (null !== $captchaAdapter) {
             $this->captchaAdapter = $captchaAdapter;
         }
-        $this->csrfToken = $csrfToken;
 
         $this->init();
     }
 
     public function init()
     {
-        $factory = new FormFactory();
         $name = $this->getName();
         if (null === $name) {
             $this->setName('contact');
         }
         
-        $this->add($factory->createElement(array(
+        $this->add(array(
             'name' => 'from',
             'attributes' => array(
                 'label' => 'From:',
                 'type'  => 'text',
             ),
-        )));
+        ));
 
-        $this->add($factory->createElement(array(
+        $this->add(array(
             'name'  => 'subject',
             'attributes' => array(
                 'label' => 'Subject:',
                 'type'  => 'text',
             ),
-        )));
+        ));
 
 
-        $this->add($factory->createElement(array(
+        $this->add(array(
             'name'  => 'body',
             'attributes' => array(
                 'label' => 'Your message:',
                 'type'  => 'textarea',
             ),
-        )));
+        ));
 
-        $this->add($factory->createElement(array(
-            'name' => 'captcha',
-            'attributes' => array(
-                'label'   => 'Please verify you are human.',
-                'captcha' => $this->captchaAdapter,
-            ),
-        )));
+        $captcha = new Element\Captcha('captcha');
+        $captcha->setCaptcha($this->captchaAdapter);
+        $captcha->setAttribute('label', 'Please verify you are human.');
+        $this->add($captcha);
 
-        /*
-         * Omitting until we have a CSRF validator
-        $this->add($factory->createElement(array(
-            'name' => 'csrf',
-            'attributes' => array(
-                'type'  => 'hidden',
-                'value' => $this->csrfToken,
-            ),
-        )));
-         */
+        $this->add(new Element\Csrf('csrf'));
 
         $this->add($factory->createElement(array(
             'name' => 'Send',
